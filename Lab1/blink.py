5e6f5ca01735ed223c186390dc94de9d232dbba3
BuildTools <unconfigured@null.spigotmc.org>
2021-01-25 17:04:48 -0800

    lab 1 files

---
+++ Lab1/blink.py
@@ -0,0 +1,15 @@
+from board import LED
+from machine import Pin
+import time
+
+#Initialize build in LED Pin
+led = Pin(LED, mode=Pin.OUT)
+t = 0
+while True:
+    led(1)
+    time.sleep(1)
+    led(0)
+    time.sleep(1)
+    t+=1
+    if t == 10:
+        break
