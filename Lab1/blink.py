690cc1363513936fd6b6b78506f005959045abcb
BuildTools <unconfigured@null.spigotmc.org>
2021-02-03 15:58:35 -0800

    updated blink, added lab 2 files

--- Lab1/blink.py
+++ Lab1/blink.py
@@ -5,11 +5,10 @@ import time
 #Initialize build in LED Pin
 led = Pin(LED, mode=Pin.OUT)
 t = 0
-while True:
+while (t <= 10):
     led(1)
-    time.sleep(.1)
+    time.sleep(1)
     led(0)
-    time.sleep(.1)
+    time.sleep(1)
     t+=1
-    if t == 5:
-        break
+    
