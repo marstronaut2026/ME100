da60a9fb81c08be1a2aa120962b5ef3c2e9aa195
BuildTools <unconfigured@null.spigotmc.org>
2021-02-03 18:15:08 -0800

    Lab 2 file

---
+++ Lab2/measure_solar.py
@@ -0,0 +1,21 @@
+from ina219 import INA219
+from machine import I2C, Pin
+from board import SDA, SCL
+import time
+
+i2c = I2C(id=0, scl=Pin(SCL), sda=Pin(SDA), freq=100000)
+
+print("scanning I2C bus ...")
+print("I2C:", i2c.scan())
+
+SHUNT_RESISTOR_OHMS = 0.1
+ina = INA219(SHUNT_RESISTOR_OHMS, i2c)
+ina.configure()
+
+while True:
+    ''' Insert your code here to read and print voltage, current, and power from ina219. '''
+    # amps = ina.current()
+    # volts = ina.voltage()
+    # power = ina.power()
+    # print("Current (A): %f \tVoltage (V): %f \tPower (W): %f" %amps %volts %power)
+    # time.sleep(0.5)
