8cc7fe39b33e2b227835fc96fa51d2f47d23b053
marstronaut2026 <austinjt@berkely.edu>
2021-07-19 14:21:12 -0700

    added comments to move.py

--- robotRD/drive_v1.py
+++ robotRD/drive_v1.py
@@ -3,32 +3,15 @@ import time
 import move
 from move import Bot
 
-# pinsR = [14, 32, 15, 33]
-# pinsL = [21, 17, 16, 19]
-
-# mR = Motor(pinsR,1,'r',0.040)
-# mR.rpm = 10
-# mL = Motor(pinsL,1,'l',0.040)
-# mL.rpm = 10
-
-# # print('{}, {}, {}'.format(mR.deg_per_step, mR.steps_per_rev, mR._T))
-
-# mR.move_frd(0.1)
-# mL.move_frd(0.1)
+# drive_v1 provides basic control functionality of
+# [INSERT NAME] bot. Allows user to drive forward/backward
+# and turn with simple inputs.
 
 pins = [14, 32, 15, 33,
         21, 17, 16, 19]
 
 b = Bot(pins, 0.040, .12)
 b.rpm = 25
-
-# b.move_frd(.10)
-# time.sleep(1)
-# b.move_bkd(.10)
-# time.sleep(1)
-# b.turn(90)
-# time.sleep(1)
-# b.turn(-90)
 
 while True:
     print('Enter command: ')
@@ -46,3 +29,26 @@ while True:
     else:
         print('Invalid command.')
     time.sleep(1)
+
+
+# pinsR = [14, 32, 15, 33]
+# pinsL = [21, 17, 16, 19]
+
+# mR = Motor(pinsR,1,'r',0.040)
+# mR.rpm = 10
+# mL = Motor(pinsL,1,'l',0.040)
+# mL.rpm = 10
+
+# # print('{}, {}, {}'.format(mR.deg_per_step, mR.steps_per_rev, mR._T))
+
+# mR.move_frd(0.1)
+# mL.move_frd(0.1)
+
+
+# b.move_frd(.10)
+# time.sleep(1)
+# b.move_bkd(.10)
+# time.sleep(1)
+# b.turn(90)
+# time.sleep(1)
+# b.turn(-90)
--- robotRD/move.py
+++ robotRD/move.py
@@ -2,10 +2,18 @@ from machine import Pin
 import time
 from math import pi
 
+author: nuclearwalrus
+credit: Stephen C. Philips http://scphillips.com 
+This code is my own, however some functions were inspired by the above. 
+Feel free to use as you please. Credit would be appreciated. 
 
+Classes for defining motors and bots using 6-pin stepper motors. 
+
+
+
+#custom error handling just because
 class Error(Exception):
     pass
-
 
 class InputError(Error):
     def __init__(self, expression, message):
@@ -14,7 +22,15 @@ class InputError(Error):
 
 
 class Motor:
-    def __init__(self, pins, mode, dir, size=1):
+    def __init__(self, pins, mode, dir, wheel_diameter=1):
+        # pins = list of pin numbers from microcontroller used for stepper control
+        # mode: 
+        #       1: Full step - high torque + high power
+        #       2: Half step - low torque + low power (NOT YET FULLY OPERATIONAL)
+        #       3: Microstep - very low torque + high accuracy (DO AT SOME POINT)
+        # dir: bot attribute, 'r' = right side motor, 'l' = left side motor
+        # wheel_diameter: bot attribute, wheel diameter (in m - default=1m because why not)
+
         self.p1 = Pin(pins[0], Pin.OUT)
         self.p2 = Pin(pins[1], Pin.OUT)
         self.p3 = Pin(pins[2], Pin.OUT)
@@ -24,9 +40,9 @@ class Motor:
         if self.dir != 'l' and self.dir != 'r':
             raise InputError("You\'re lost",
                              "{}: invalid wheel orientation".format(self.dir))
-        if self.mode == 1:  # Full Step
+        if self.mode == 1:
             self.deg_per_step = 5.625 / 32
-        elif self.mode == 2:  # Half Step
+        elif self.mode == 2:
             self.deg_per_step = 5.625 / 64
         else:
             raise InputError("cat.exe has failed",
@@ -34,7 +50,7 @@ class Motor:
 
         self.steps_per_rev = int(360/self.deg_per_step)
         self.step_angle = 0
-        self.wheel_diameter = size
+        self.wheel_diameter = wheel_diameter
 
     def _set_rpm(self, rpm):
         self._rpm = rpm
@@ -43,6 +59,9 @@ class Motor:
     rpm = property(lambda self: self._rpm, _set_rpm)
 
     def move_deg(self, angle):
+        # move motor to a specific orientation (in deg) 
+        # 0 deg is always motor's original position
+
         target_step_angle = 8*(int(angle/self.deg_per_step)/8)
         steps = target_step_angle-self.step_angle
         steps = (steps % self.steps_per_rev)
@@ -63,19 +82,19 @@ class Motor:
                 self._move_cw_2(steps / 8)
         self.step_angle = target_step_angle
 
-    def move_frd(self, distance):
-        angle = distance/(pi*self.wheel_diameter)*360
-        target_step_angle = 8*(int(angle/self.deg_per_step)/8)
-        steps = target_step_angle-self.step_angle
-        # print("moving " + str(steps) + " steps")
-        if self.mode == 1:
-            if self.dir == 'r':
-                self._move_cw_1(steps / 4)
-            else:
-                self._move_acw_1(steps / 4)
-        else:
-            self._move_cw_2(steps / 8)
-        self.step_angle = target_step_angle
+    # def move_frd(self, distance):
+    #     angle = distance/(pi*self.wheel_diameter)*360
+    #     target_step_angle = 8*(int(angle/self.deg_per_step)/8)
+    #     steps = target_step_angle-self.step_angle
+    #     # print("moving " + str(steps) + " steps")
+    #     if self.mode == 1:
+    #         if self.dir == 'r':
+    #             self._move_cw_1(steps / 4)
+    #         else:
+    #             self._move_acw_1(steps / 4)
+    #     else:
+    #         self._move_cw_2(steps / 8)
+    #     self.step_angle = target_step_angle
 
     def __clear(self):
         self.p1.value(0)
@@ -117,6 +136,7 @@ class Motor:
                 time.sleep(self._T*2)
         self.__clear()
 
+    # INTERNAL FUNCTIONS FOR HALF STEPPING AND MICROSTEPPING TBD
     def _move_acw_2(self, big_step):
         pass
 
@@ -125,64 +145,85 @@ class Motor:
 
 
 class Bot:
-    def __init__(self, pins, size=1, width=1):
+    def __init__(self, pins, wheel_diameter=1, width=1):
+        # currently set up for 2 wheeled bots driven by 6-pin stepper motors
+        # motors currently only run in full stepping mode (i liek tawwk)
+        # pins must be formatted as:
+        # [right, side, motor, pins, left, side, motor, pins]
+        # order 
+        # wheel_diameter = wheel diameter
+        # width = distance between bot wheels
+
+        # right side motor pins
         self.p1 = Pin(pins[0], Pin.OUT)
         self.p2 = Pin(pins[1], Pin.OUT)
         self.p3 = Pin(pins[2], Pin.OUT)
         self.p4 = Pin(pins[3], Pin.OUT)
+
+        # left side motor pins
         self.p5 = Pin(pins[4], Pin.OUT)
         self.p6 = Pin(pins[5], Pin.OUT)
         self.p7 = Pin(pins[6], Pin.OUT)
         self.p8 = Pin(pins[7], Pin.OUT)
 
+        # Possibility of adding individual motor control at some point
         # rPins = pins[0:3]
         # lPins = pins[4:7]
 
-        # mR = Motor(rPins,1,'r',size)
-        # mL = Motor(lPins,1,'l',size)
+        # mR = Motor(rPins,1,'r',wheel_diameter)
+        # mL = Motor(lPins,1,'l',wheel_diameter)
 
-        self.deg_per_step = 5.625 / 32
+        self.deg_per_step = 5.625 / 32  # full stepping
         self.steps_per_rev = int(360/self.deg_per_step)
-        self.step_angle = 0
-        self.wheel_diameter = size
+        self.step_angle = 0  # current angle based off initial position of motors
+        self.last_step_angle = 0  # motor positions after last action - use for
+        # position control at some point
+        self.wheel_diameter = wheel_diameter
         self._botRad = width / 2
 
     def _set_rpm(self, rpm):
         self._rpm = rpm
         self._T = (60.0 / rpm)/self.steps_per_rev
 
+    # allows for changing rpm speed
     rpm = property(lambda self: self._rpm, _set_rpm)
 
     def move_frd(self, distance):
+        # robot drives forward a certain distance (in m) specified by user.
+        # ie: distance = 1 means robot will drive forward 1 meter.
         angle = distance/(pi*self.wheel_diameter)*360
         target_step_angle = 8*(int(angle/self.deg_per_step)/8)
         steps = target_step_angle-self.step_angle
         # print("moving " + str(steps) + " steps")
 
-        self._move_fw(steps / 4)
+        self._move_fw(steps / 4)  # divide by 4 because full stepping motors
 
         self.step_angle = 0
 
     def move_bkd(self, distance):
+        # robot drives backward a certain distance (in m) specified by user.
+        # ie: distance = 1 means robot will drive forward 1 meter.
         angle = distance/(pi*self.wheel_diameter)*360
         target_step_angle = 8*(int(angle/self.deg_per_step)/8)
         steps = target_step_angle-self.step_angle
         # print("moving " + str(steps) + " steps")
 
-        self._move_bk(steps / 4)
+        self._move_bk(steps / 4)  # divide by 4 because full stepping motors
 
         self.step_angle = 0
 
     def turn(self, theta):
+        # turns robot an angle (in deg) specified by user
+
         distance = self._botRad*abs(theta*pi/180)
         angle = distance/(pi*self.wheel_diameter)*360
         target_step_angle = 8*(int(angle/self.deg_per_step)/8)
         steps = target_step_angle-self.step_angle
 
         if theta > 0:
-            self._turn_r(steps / 4)
+            self._turn_r(steps / 4)  # divide by 4 because full stepping motors
         elif theta < 0:
-            self._turn_l(steps / 4)
+            self._turn_l(steps / 4)  # divide by 4 because full stepping motors
         else:
             pass
 
@@ -200,7 +241,7 @@ class Bot:
 
     def _move_fw(self, big_step):
         self.__clear()
-        driveSeq = [
+        driveSeq = [  # pin activation cycle for full stepping motor
             [1, 1, 0, 0],
             [1, 0, 0, 1],
             [0, 0, 1, 1],
